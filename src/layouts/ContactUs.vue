5e7c3547f1bf414a8e2e07ccb172ae2ab5c81181
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-09-08 00:05:02 +0600

    Etosha app build finished
    initially finished

--- src/layouts/ContactUs.vue
+++ src/layouts/ContactUs.vue
@@ -1,22 +1,33 @@
 <template>
     <Navbar/>
-  <h1>Contact Us</h1>
-  <h1>Contact Us</h1>
-  <h1>Contact Us</h1>
-  <h1>Contact Us</h1>
-  <h1>Contact Us</h1>
-  <h1>Contact Us</h1>
-  <h1>Contact Us</h1>
+    <ContactForm/>
+    <GetProductUpdates/>
     <Footer/>
 </template>
 
 <script>
 import Navbar from 'src/components/Navbar.vue'
 import Footer from 'src/components/Footer.vue'
+import ContactForm from 'src/subComponent/ContactForm.vue'
+import GetProductUpdates from 'src/subComponent/GetProductUpdates.vue'
+import { useMeta } from 'quasar'
+
+let metaData = {
+  // sets document title
+  title: 'Etosha | Contact Us',
+  // optional; sets final title as "Index Page - My Website", useful for multiple level meta
+  titleTemplate: title => `${title}`,
+  // meta tags
+  meta: {
+    description: { name: 'theme-color', content: '#cb945e'}
+  }
+}
 
 export default {
-  components: { Navbar, Footer },
-
+  components: { Navbar, Footer, ContactForm, GetProductUpdates },
+  setup() {
+    useMeta(metaData)
+  }
 }
 </script>
 
